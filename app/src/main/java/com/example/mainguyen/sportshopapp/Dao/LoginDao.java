ce1385f2accb2a6a33cf4763dce738c5079942e8
PN\mai.nguyen <mai.nguyen@student.passerellesnumeriques.org>
2017-04-11 14:15:34 +0700

    newsomefile

---
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Dao/LoginDao.java
@@ -0,0 +1,8 @@
+package com.example.mainguyen.sportshopapp.Dao;
+
+/**
+ * Created by mai.nguyen on 11/04/2017.
+ */
+
+public class LoginDao {
+}
---
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Models/StaffInformationModel.java
@@ -0,0 +1,8 @@
+package com.example.mainguyen.sportshopapp.Models;
+
+/**
+ * Created by mai.nguyen on 11/04/2017.
+ */
+
+public class StaffInformationModel {
+}
