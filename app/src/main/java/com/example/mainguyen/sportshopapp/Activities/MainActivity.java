afb74ef1448a69fe66dd7d0ccbad7dc63b188668
PN\mai.nguyen <mai.nguyen@student.passerellesnumeriques.org>
2017-04-11 13:52:20 +0700

    buildproject

--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.mainguyen.sportshopapp;
+package com.example.mainguyen.sportshopapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.mainguyen.sportshopapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
