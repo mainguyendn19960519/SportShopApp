374a499e356c6085cac7fdb771dc5b6730179b34
HaDinhThanh <ha.dinh@student.passerellesnumeriques.org>
2017-04-14 13:43:24 +0700

    onclicktoform

--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
@@ -1,8 +1,32 @@
 package com.example.mainguyen.sportshopapp.Activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mainguyen.sportshopapp.R;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btNextScreenLogin =(Button) findViewById(R.id.btLogin);
+
+        btNextScreenLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), UserActivity.class);
+               // Log.e("n", inputName.getText() + "." + inputEmail.getText());
+
+                startActivity(nextScreen);
+
+            }
+        });
 
 
+    }
 }
--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/UserActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/UserActivity.java
@@ -1,7 +1,13 @@
 package com.example.mainguyen.sportshopapp.Activities;
 
+import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mainguyen.sportshopapp.R;
 
@@ -11,5 +17,31 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Intent i = getIntent();
+        // Receiving the Data
+        Button btnLogout=(Button) findViewById(R.id.btn_logout);
+        Button btnLogin=(Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0) {
+//             if (textUserName.getText().equals("admin") && textPassWord.getText().equals("123456"))
+//               {
+                    Intent nextMNScreen = new Intent(getApplication(), ManagementActivity.class);
+                   // Log.e("n", textUserName.getText() + ".=======================" + textPassWord.getText());
+                    startActivity(nextMNScreen);
+//                }else {
+//                    Intent nextScreen = new Intent(getApplication(), UserActivity.class);
+//                    startActivity(nextScreen);
+//               }
+
+            }
+});
+        // Binding Click event to Button
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Closing Main2Activity
+                finish();
+            }
+        });
     }
 }
