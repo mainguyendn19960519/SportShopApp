ae951c34bddf995265fc37cec9269fece6a8deea
PN\da.nguyen <da.nguyen@student.passerellesnumeriques.org>
2017-04-18 17:13:01 +0700

    finishform

--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/UserActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/UserActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
@@ -16,33 +18,23 @@ public class UserActivity extends AppCompatActivity {
     EditText edEmail;
     EditText edPass;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Intent i = getIntent();
         // Receiving the Data
-        Button btnLogout=(Button) findViewById(R.id.btn_logout);
-        btnLogin.setOnClickListener(new View.OnClickListener(){
+        Button btnLogout=(Button) findViewById(R.id.btn_cancel);
+        btnLogout.setOnClickListener(new View.OnClickListener(){
             public void onClick(View arg0) {
-//             if (textUserName.getText().equals("admin") && textPassWord.getText().equals("123456"))
-//               {
-                    Intent nextMNScreen = new Intent(getApplication(), ManagementActivity.class);
-                   // Log.e("n", textUserName.getText() + ".=======================" + textPassWord.getText());
-                    startActivity(nextMNScreen);
-//                }else {
-//                    Intent nextScreen = new Intent(getApplication(), UserActivity.class);
-//                    startActivity(nextScreen);
-//               }
 
+                    Intent nextMNScreen = new Intent(getApplication(), ManagementActivity.class);
+                    startActivity(nextMNScreen);
             }
 });
-        // Binding Click event to Button
         btnLogout.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
-                //Closing Main2Activity
                 finish();
             }
         });
@@ -50,10 +42,10 @@ public class UserActivity extends AppCompatActivity {
         edEmail = (EditText) findViewById(R.id.tv_inputEmai);
         edPass = (EditText) findViewById(R.id.tv_inputPassword);
 
-
         Button btnlogin= (Button) findViewById(R.id.btn_login);
 
         Button btncancel = (Button) findViewById(R.id.btn_cancel);
+
         btnlogin.setOnClickListener(new View.OnClickListener() {
 
             public  void onClick(View arg0){
@@ -64,7 +56,6 @@ public class UserActivity extends AppCompatActivity {
                 }
                 else if (!edEmail.getText().toString().contains("@") || !edEmail.getText().toString().contains(".")){
                     edEmail.setError("Please check your email! ");
-
                     edEmail.requestFocus();
                 }
                 else if(!edEmail.getText().toString().matches(email)){
@@ -77,10 +68,11 @@ public class UserActivity extends AppCompatActivity {
                 }
                 else if(edPass.getText().toString().length() < 8  ){
                     edPass.setError("Password should be atleast of 8 charactors!");
-
                     edPass.requestFocus();
+                } else if (edEmail.getText().toString().equals("danguyen@gmail.com") && edPass.getText().toString().equals("123456789")) {
+                    Intent nextScreen = new Intent(getApplicationContext(), ManagementActivity.class);
+                    startActivity(nextScreen);
                 }
-
             }
         });
         btncancel.setOnClickListener(new View.OnClickListener() {
