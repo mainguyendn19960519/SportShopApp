7819e0708e7d328490b96056641f0ec2912eeccf
PN\da.nguyen <da.nguyen@student.passerellesnumeriques.org>
2017-04-20 10:15:36 +0700

    backgroud

--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/ManagementActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/ManagementActivity.java
@@ -1,7 +1,10 @@
 package com.example.mainguyen.sportshopapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mainguyen.sportshopapp.R;
 
@@ -9,10 +12,18 @@ import com.example.mainguyen.sportshopapp.R;
  * Created by ha.dinh on 4/12/2017.
  */
 
-public class ManagementActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_management);
+    public class ManagementActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_management);
+            Button btNextScreenLogin =(Button) findViewById(R.id.btn_userManagement);
+
+                    btNextScreenLogin.setOnClickListener(new View.OnClickListener(){
+                       public void onClick(View arg0) {
+                           Intent nextScreen = new Intent(getApplicationContext(), UserManagementActivity.class);
+                           startActivity(nextScreen);
+                       }
+                    });
+        }
     }
-}
