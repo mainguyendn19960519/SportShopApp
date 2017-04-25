217f36831344d71f959b9afe9962d9bfc35f87fe
PN\da.nguyen <da.nguyen@student.passerellesnumeriques.org>
2017-04-25 14:10:45 +0700

    editButton

--- app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/mainguyen/sportshopapp/Activities/MainActivity.java
@@ -3,25 +3,56 @@ package com.example.mainguyen.sportshopapp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.mainguyen.sportshopapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+    @Override
+    protected void onResume() {
+        Log.v("Test onResume","Test onResume");
+        super.onResume();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btNextScreenLogin =(Button) findViewById(R.id.btLogin);
-        btNextScreenLogin.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0) {
-                Intent userActivity = new Intent(getApplicationContext(), UserActivity.class);
-                startActivity(userActivity);
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("Home").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Login").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Search").setTabListener(this));
   }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        int nTabSelected = tab.getPosition();
+        switch(nTabSelected){
+            case 0:
+                setContentView(R.layout.activity_main);
+                break;
+            case 1:
+                Intent nextScreen = new Intent(getApplicationContext(), UserActivity.class);
+                startActivity(nextScreen);
+                break;
+            case 2:
+                setContentView(R.layout.activity_search);
+                break;
+        }
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
 }
